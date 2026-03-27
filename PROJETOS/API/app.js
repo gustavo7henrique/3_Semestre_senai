4bb636ece32132335e82d6290a578e960960408c
GustavoH <gustavoh.npinheiro2009@gmail.com>
2026-03-27 15:36:25 -0300

    Push 27/03

--- PROJETOS/API/app.js
+++ PROJETOS/API/app.js
@@ -1,29 +1,27 @@
 import express from 'express';
-// import rotasUsuarios from "./src/routes/rotasUsuarios.js";
+import rotasUsuarios from "./src/routes/rotasUsuarios.js";
 
 
 //Importando o Banco de Dados
 import { BD, testarConexao } from "./db.js";
 
 //Importando Swagger
-// import swaggerUI from "swagger-ui-express";
-// import documentacao from "./config/swagger.js";
+import swaggerUI from "swagger-ui-express";
+import documentacao from "./config/swagger.js";
 import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-// app.use('/swagger', swaggerUI.serve, swaggerUI.setup(documentacao));
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(documentacao));
 
 app.get('/', async (req, res) => {
     await testarConexao();
     // res.status(200).json('API FUNCIONANDO ✅');
-    // res.redirect('/swagger')
+    res.redirect('/swagger')
 });
 
 //Utilizando Rotas
 app.use(rotasUsuarios);
-app.use(rotasDepartamentos);
-app.use(rotasOrdemServicos);
 app.use(cors());
 
 const porta = 3000;
